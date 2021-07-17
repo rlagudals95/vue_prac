1a65b03ee30b3b49f332f69cafd46c9a2405767e
unknown <you@example.com>
2021-07-18 00:25:32 +0900

    게시물 모달 컴포넌트 세부화 진행중

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
+  <!-- props로 close라는 함수를 넘겨준다 -->
+
   <h1>원룸팜</h1>
 
   <div class="menu">
     <!-- menus의 -->
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
-  <div class="items" v-for="(item, i) in items" :key="i">
-    <img class="image" :src="require(`${items[i].image}`)" />
-    <h3>{{ items[i].name }}</h3>
-    <p>{{ items[i].price }}만원</p>
+  <div class="items" v-for="(item, i) in datas" :key="i">
+    <img class="image" :src="datas[i].image" @click="modalOpen = true" />
+    <h3>{{ datas[i].title }}</h3>
+    <p>{{ datas[i].price }}만원</p>
+    <p>{{ datas[i].content }}</p>
+    <modal
+      v-if="modalOpen"
+      @close="modalOpen = false"
+      :info="this.datas[i]"
+    ></modal>
+
     <!-- 버튼에 코드 달아주기 매우 직관적이다..! -->
-    <button class="btn" @click="items[i].count++" @mouseover="test">
+    <!-- <button class="btn" @click="items[i].count++" @mouseover="test">
       허위매물 신고
     </button>
-    <span>신고수 : {{ items[i].count }}</span>
+    <span>신고수 : {{ items[i].count }}</span> -->
   </div>
 </template>
 
 <script>
+import oneRooms from "./data";
+import modal from "./components/Modal.vue";
+console.log("원룸데이터", oneRooms);
+
 export default {
   name: "App",
   data() {
     // data 보관함 변수 같은 것들을 여기에 다 보관한다고 보면 된다
 
     return {
+      datas: oneRooms,
+      modalOpen: false,
       menus: ["Home", "Products", "About"],
       style: "color:red", // style도 데이터바인딩 가능 대신 : 붙여줘야한다
       items: [
@@ -55,9 +70,13 @@ export default {
       // 호출하는 대상의 count를 증가 this를 써주자
       this.count++;
     },
+
+    closeModal() {
+      this.modalOpen = false;
+    },
   },
 
-  components: {},
+  components: { modal },
 };
 </script>
 
@@ -71,6 +90,32 @@ export default {
   margin-top: 60px;
 }
 
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.1);
+  position: fixed;
+  top: 0;
+}
+
+.white-bg {
+  margin: 0 auto;
+  width: 80%;
+  background-color: white;
+  border-radius: 8px;
+  padding: 20px;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
 .menu {
   background: darkslateblue;
   padding: 15px;
---
+++ src/components/Modal.vue
@@ -0,0 +1,76 @@
+
+<template>
+  <div class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <!-- 전달받은 함수를 이런 형식으로 써준다 아래 props 타입설정 -->
+      <button @click="$emit('close')">닫기</button>
+      {{ info.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "modal",
+  data() {
+    return {
+      title: void 0,
+      modal: true,
+    };
+  },
+  methods: {},
+  props: {
+    // props 타입을 설정안해줘도 오류는 안뜬다...
+    close: Function,
+    info: Object,
+  },
+};
+</script>
+
+<style >
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+}
+
+.white-bg {
+  margin: 0 auto;
+  width: 80%;
+  background-color: white;
+  border-radius: 8px;
+  padding: 20px;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.btn {
+  margin-right: 1rem;
+}
+
+.image {
+  width: 100%;
+  background-size: cover;
+}
+</style>
